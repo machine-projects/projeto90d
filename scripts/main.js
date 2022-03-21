4548ab4014b9cd9d00179a9972b608efc5e8b076
Felipe Toffoli <felipe_toffoli1@users.noreply.github.com>
2022-03-21 10:38:34 -0400

    duvidas stlyle e js

---
+++ scripts/main.js
@@ -0,0 +1,16 @@
+function showOrHideElement(element) {
+  let getElement = document.querySelector(`${element}`).classList;
+  let insHide = false;
+  getElement.forEach((e) => {
+    if (e == "collapse") insHide = true;
+  });
+
+  switch (insHide) {
+    case true:
+      getElement.remove("collapse");
+      break;
+    case false:
+      getElement.add("collapse");
+      break;
+  }
+}
